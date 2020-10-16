8b0f8ba0d2ce9b7f6e95f9fb9ccb0405322b0718
HunorMarton <bor.hunor@gmail.com>
2020-10-16 17:57:20 +0200

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Card.css';
 
-function App() {
+function Card() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Card">
+      <h3>Rick & Morty</h3>
+      <p>Animation</p>
     </div>
   );
 }
 
-export default App;
+export default Card;
