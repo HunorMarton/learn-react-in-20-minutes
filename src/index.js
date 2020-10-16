b6ff2d62d4c3a5065a7fa4a99bdd7b2a916297ec
HunorMarton <bor.hunor@gmail.com>
2020-10-16 17:57:20 +0200

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
