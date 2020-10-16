431db55f56bdb1127312d9db1cbd5fd0f5026800
HunorMarton <bor.hunor@gmail.com>
2020-10-16 18:28:02 +0200

    Methods

--- src/Counter.js
+++ src/Counter.js
@@ -4,11 +4,14 @@ import './Counter.css';
 function Counter() {
   const [counter, setCounter] = useState(0)
 
+  const decrease = () => setCounter(counter - 1)
+  const increase = () => setCounter(counter + 1)
+
   return (
     <div className="Counter">
       <h3>{counter}</h3>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
     </div>
   );
 }
